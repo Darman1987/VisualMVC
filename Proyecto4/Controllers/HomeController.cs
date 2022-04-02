559031cf4a6fbf10ea0880a0fd20fcdeb74f3c3f
Darman1987 <darman1987@hotmail.com>
2022-04-02 21:49:02 +1000

    libro de visitas a un sitio web

---
+++ Proyecto4/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Proyecto4.Models; //requerida para usar la clase creada en Modelos LibroVisitas
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();  //obtener datos enviados por post
+            string comentarios = Request.Form["comentarios"].ToString();
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["libro"] = todo;
+            //Mediante el diccionario ViewData podemos pasar datos a la vista, creamos una entrada en el diccionario
+            //y le asignamos el string recuperado desde el "Modelo":
+            return View();
+        }
+    }
+}
---
+++ Proyecto4/Models/LibroVisitas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;//se requiere para usar StreamWriter
+using System.Linq;
+using System.Web;
+using System.Web.Hosting; // se requiere para usar HostingEnvironment
+
+namespace Proyecto4.Models
+{
+    public class LibroVisitas
+    {
+        public void Grabar(string nombre, string comentarios)
+        {
+            StreamWriter archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
+            //el método MapPath del objeto HostingEnvironment que nos devuelve la ruta donde se almacena nuestro proyecto
+            archivo.WriteLine("Nombre:" + nombre + "<br>Comentarios:" + comentarios + "<hr>");
+            //el método WriteLine permite escribir en el archivo
+            archivo.Close();
+        }
+
+        public string Leer()
+        {
+            StreamReader archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            //el método MapPath del objeto HostingEnvironment que nos devuelve la ruta donde se almacena nuestro proyecto
+            string todo = archivo.ReadToEnd();
+           // el método "ReadToEnd" recuperamos todos los caracteres almacenados en el archivo de texto
+            archivo.Close();
+            return todo;
+        }
+    }
+}
